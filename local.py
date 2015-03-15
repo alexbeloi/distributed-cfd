545d6b35de4f450b19d7e5a7859a78707ebcca21
Alex Beloi <beloi.alex@gmail.com>
2015-03-14 20:17:07 -0700

    test: higher resolution

--- local.py
+++ local.py
@@ -7,8 +7,8 @@ const_range = 2*math.pi
 const_cfl = 0.9
 const_stensize = 2
 const_dim = 2
-const_num_cells = 128
-const_block_size = 16
+const_num_cells = 1024
+const_block_size = 128
 
 const_ax = -0.7
 const_ay = -0.2
